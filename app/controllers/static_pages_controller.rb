5a3da5e54186fc0af08308e92979d51a5ee62dea
Peppermill <reidaloud@gmail.com>
2013-04-21 14:26:16 -0400

    Add as StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+# note that the controller is a class
+class StaticPagesController < ApplicationController
+	# this is an action.  although it looks empty,
+	# it is inheriting from ApplicationsController.
+	# the action it renders is to inherit the view (the template)
+  def home
+  end
+	# this is an action
+  def help
+  end
+end
